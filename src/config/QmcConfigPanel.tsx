bf4174f933e520a2a71d7e05fd691fca960a18bf
Jixun Wu <jixun.moe@gmail.com>
2022-11-07 22:29:12 +0000

    fix: add label for qmc2 seed

--- src/config/QmcConfigPanel.tsx
+++ src/config/QmcConfigPanel.tsx
@@ -43,23 +43,22 @@ export function QmcConfigPanel() {
     <StyledTabPanel>
       <StyledSectionPanel title="QMC2">
         <FormLabel>
+          <Text>密钥解密种子</Text>
           <NumberInput value={qmc2.seed} min={0} max={255} onChange={storeSeed}>
             <NumberInputField />
           </NumberInput>
         </FormLabel>
 
-        <StyledSectionPanel title="QMC2 - EncV2 选项" titleSize="sm">
-          <HexInput
-            value={qmc2.stage_1_key}
-            onChange={storeStage1Key}
-            title="阶段 1 密钥"
-          />
-          <HexInput
-            value={qmc2.stage_2_key}
-            onChange={storeStage2Key}
-            title="阶段 2 密钥"
-          />
-        </StyledSectionPanel>
+        <HexInput
+          value={qmc2.stage_1_key}
+          onChange={storeStage1Key}
+          title="(EncV2) 阶段 1 密钥"
+        />
+        <HexInput
+          value={qmc2.stage_2_key}
+          onChange={storeStage2Key}
+          title="(EncV2) 阶段 2 密钥"
+        />
       </StyledSectionPanel>
     </StyledTabPanel>
   );
